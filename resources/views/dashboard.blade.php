f3b4e8c8320728c28169b21f9a4c5a0157e012fc
DaiNaka1207 <wxfpk432@gmail.com>
2024-03-30 17:33:47 +0900

    :sparkles: Add menu to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
+    <div class="mt-2 max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            新規作成
+        </button>
     </div>
 </x-app-layout>
