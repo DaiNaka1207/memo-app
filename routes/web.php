28202e318c5d2f7879fe6e025972d0d0e18c8ac4
DaiNaka1207 <wxfpk432@gmail.com>
2024-04-14 21:56:50 +0900

    :sparkles: Add memo register screen

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MemoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('memo', MemoController::class);
 
 Route::middleware([
     'auth:sanctum',
